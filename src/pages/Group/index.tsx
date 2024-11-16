ac7be0950c560bfff0751ae109eb32ddfba6a6d8
Lucas <lgrati@fi.uba.ar>
2024-11-16 19:59:22 -0300

    wip

--- src/pages/Group/index.tsx
+++ src/pages/Group/index.tsx
@@ -1,15 +1,71 @@
-import { useLocation } from "react-router-dom";
-import { Feed, Container } from "./styled";
+import { useState } from "react";
+import { Title } from "../SearchBar/styled";
+import { Container, RightSection, SectionTitle } from "./styled";
 
-export const Group = () => {
+import { useLocation, useNavigate } from "react-router-dom";
+import { useSnackbar } from "notistack";
+
+export const defaultPhotoUrl =
+  "https://firebasestorage.googleapis.com/v0/b/buena-leida.appspot.com/o/profiles%2Fdefault.jpg?alt=media&token=100a1fe2-fd46-4fc5-9d11-e7b78ed946f5";
+
+export const GroupSearch = () => {
+  const { enqueueSnackbar } = useSnackbar();
+  const navigate = useNavigate();
+  const [isLoading, setIsLoading] = useState(false);
+
   const location = useLocation();
   const groupId = location.state?.query || "";
 
   return (
     <Container>
-      <Feed>Group Search {groupId}</Feed>
+      <div style={{ minWidth: "600px" }}>
+        <Title>Grupo id: {groupId}</Title>
+      </div>
+      <RightSection>
+        <SectionTitle>Creador:</SectionTitle>
+      </RightSection>
+
+      {/* Adapatar a dialogo de crear topico <Dialog open={showCreateGroupModal} onClose={handleClose}>
+        <DialogTitle>Crear Grupo</DialogTitle>
+        <DialogContent>
+          <FormControl fullWidth margin="normal">
+            <TextField
+              label="Group name"
+              value={groupName}
+              onChange={(e) => setGroupName(e.target.value.slice(0, 20))}
+              helperText={`${groupName.length}/20`}
+              slotProps={{
+                htmlInput: { maxLength: 20 },
+              }}
+              variant="outlined"
+            />
+          </FormControl>
+          <StyledDialogContent>
+            {availableCategories.map((category) => (
+              <StyledFormControl key={category}>
+                <FormControlLabel
+                  control={
+                    <Checkbox
+                      checked={selectedCategories.indexOf(category) !== -1}
+                      onChange={() => handleCategoryChange(category)}
+                      color="primary"
+                    />
+                  }
+                  label={category}
+                />
+              </StyledFormControl>
+            ))}
+          </StyledDialogContent>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose}>Cancelar</Button>
+          {availableCategories.length > 0 && (
+            <Button color="primary">Guardar</Button>
+          )}
+        </DialogActions>
+      </Dialog> */}
     </Container>
   );
 };
 
-export default Group;
+export default GroupSearch;
--- src/pages/Group/styled.tsx
+++ src/pages/Group/styled.tsx
@@ -1,16 +1,116 @@
+import { DialogContent, FormControl } from "@mui/material";
 import styled from "styled-components";
 
 export const Container = styled.div`
+  max-width: 1200px;
+  margin: 0 auto;
+  padding: 20px;
   display: flex;
-  flex-direction: column;
-  height: 80vh;
+  justify-content: space-between;
 `;
 
-export const Feed = styled.div`
-  flex: 1;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  font-size: 2rem;
+export const RightSection = styled.div`
+  width: 40%;
+  position: sticky;
+  top: 20px;
+  align-self: flex-start;
+`;
+
+export const CategoryGrid = styled.div`
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  gap: 5px;
+  margin-bottom: 15px;
+`;
+
+export const CategoryItem = styled.div`
+  padding: 10px;
+  border-radius: 5px;
+  text-align: center;
+  cursor: pointer;
+  transition: background-color 0.3s;
+
+  &:hover {
+    background-color: #e0e0e0;
+  }
+`;
+
+export const SectionTitle = styled.h2`
+  font-size: 1.4em;
+  margin-bottom: 20px;
+  margin-top: 30px;
+`;
+
+export const CreateGroupButton = styled.button`
+  background-color: var(--c-color);
+  border: none;
+  color: white;
+  padding: 10px 20px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 16px;
+  margin: 4px 2px;
+  cursor: pointer;
+  margin-top: 15px;
+  border-radius: 5px;
+  transition: background-color 0.3s;
+
+  &:hover {
+    background-color: var(--d-color);
+  }
+`;
+
+export const GroupCard = styled.div`
+  background: #fff;
+  border: 1px solid #e0e0e0;
+  border-radius: 8px;
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+  overflow: hidden;
+  width: 100%;
+`;
+
+export const GroupImage = styled.img`
+  width: 100%;
+  height: 150px;
+  object-fit: cover;
+  &:hover {
+    cursor: pointer;
+  }
+`;
+
+export const GroupInfo = styled.div`
+  padding: 16px;
+`;
+
+export const GroupName = styled.h3`
+  font-size: 18px;
+  margin: 0;
   color: #333;
+  &:hover {
+    cursor: pointer;
+  }
+`;
+
+export const GroupDescription = styled.p`
+  font-size: 14px;
+  color: #666;
+  margin: 8px 0 16px;
+`;
+
+export const UsersCount = styled.span`
+  font-size: 14px;
+  font-weight: bold;
+  color: #444;
+`;
+
+export const StyledDialogContent = styled(DialogContent)`
+  display: flex;
+  flex-wrap: wrap;
+  gap: 5px;
+`;
+
+export const StyledFormControl = styled(FormControl)`
+  flex: 1 1 calc(50% - 5px);
+  min-width: 60px;
 `;
