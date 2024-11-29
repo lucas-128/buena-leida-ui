d0d17c5e320474b6abee48d68b91da689be071d0
Lucas <lgrati@fi.uba.ar>
2024-11-29 16:50:29 -0300

    friends

--- src/pages/Friends/index.tsx
+++ src/pages/Friends/index.tsx
@@ -4,7 +4,6 @@ import { Typography } from "@mui/material";
 //import DeleteIcon from '@mui/icons-material/Delete';
 import { useGlobalState } from "../../context/GlobalStateContext";
 import {
-  Bio,
   DeleteButton,
   FriendCard,
   FriendInfo,
@@ -20,35 +19,31 @@ import {
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
 
-// TODO: Los nombres de los campos tienen que ser los mismos que devuelve el back
-
 interface FriendData {
-  id: number;
-  name: string;
-  username: string;
-  profileImage: string;
-  bio: string;
+  friendship: number;
+  friend: {
+    id: number;
+    username: string;
+    name: string;
+    profilePhoto: string;
+  };
 }
+
+const API_URL = "http://localhost:3000";
 
 export const Friends: React.FC = () => {
   const { state } = useGlobalState();
-  // Esto inicialmente es un array vacio.
-  // usar useEffect para fetchear los amigos del usuario
+
   const [friends, setFriends] = React.useState<FriendData[]>([]);
   const navigate = useNavigate();
 
   useEffect(() => {
     const fetchFriends = async () => {
       try {
-        const response = await axios.get(`https://buena-leida-back-kamk.onrender.com/friends/${state.id}`); // Example API call, use actual user ID
-        const friendsData = response.data.map((friend: any) => ({
-          id: friend.friendId, // Assuming `friendId` in the response
-          name: friend.name,
-          username: friend.username,
-          profileImage: friend.profileImage,
-          bio: friend.bio,
-        }));
-        setFriends(friendsData);
+        const response = await axios.get(
+          `${API_URL}/friendships/friends/${state.id}`
+        );
+        setFriends(response.data);
       } catch (error) {
         console.error("Error fetching friends:", error);
       }
@@ -60,8 +55,12 @@ export const Friends: React.FC = () => {
   // Delete friend functionality
   const handleDeleteFriend = async (friendId: number) => {
     try {
-      await axios.delete(`https://buena-leida-back-kamk.onrender.com/delete/${friendId}`);
-      setFriends((prevFriends) => prevFriends.filter((friend) => friend.id !== friendId));
+      await axios.delete(
+        `https://buena-leida-back-kamk.onrender.com/delete/${friendId}`
+      );
+      setFriends((prevFriends) =>
+        prevFriends.filter((friend) => friend.friend.id !== friendId)
+      );
     } catch (error) {
       console.error("Error deleting friend:", error);
     }
@@ -76,28 +75,26 @@ export const Friends: React.FC = () => {
             {friends.map((friend) => (
               <FriendCard
                 onClick={() =>
-                  navigate("/otherprofile", { state: { query: friend.id } })
+                  navigate("/otherprofile", {
+                    state: { query: friend.friend.id },
+                  })
                 }
-                key={friend.id}
+                key={friend.friendship}
               >
                 <ProfilePicture
-                  src={friend.profileImage}
-                  alt={`${friend.name}'s profile`}
+                  src={friend.friend.profilePhoto}
+                  alt={`${friend.friend.name}'s profile`}
                 />
                 <FriendInfo>
-                  <Name>{friend.name}</Name>
-                  <Username>{friend.username}</Username>
-                  <Bio>
-                    {friend.bio.length > 75
-                      ? `${friend.bio.slice(0, 72)}...`
-                      : friend.bio}
-                  </Bio>
+                  <Name>{friend.friend.name}</Name>
+                  <Username>{friend.friend.username}</Username>
                 </FriendInfo>
-                <DeleteButton onClick={(e) => {
-                  e.stopPropagation(); // Prevent navigating to the friend's profile
-                  handleDeleteFriend(friend.id); // Delete the friend
-                }}>
-                </DeleteButton>
+                <DeleteButton
+                  onClick={(e) => {
+                    e.stopPropagation(); // Prevent navigating to the friend's profile
+                    handleDeleteFriend(friend.friend.id); // Delete the friend
+                  }}
+                ></DeleteButton>
               </FriendCard>
             ))}
           </div>
