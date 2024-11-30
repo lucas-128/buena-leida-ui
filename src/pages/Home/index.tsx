3bea52aa1ba475c6c994b2e61f6a6ed32e88d824
Lucas <lgrati@fi.uba.ar>
2024-11-29 21:54:17 -0300

    esqueleto home

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,9 +1,220 @@
-import { Feed, HomeContainer } from "./styled";
+import React from "react";
+import {
+  HomeContainer,
+  Carousel,
+  BookCard,
+  BookCover,
+  BookTitle,
+  BookAuthor,
+  UserCard,
+  UserAvatar,
+  UserName,
+  UserUsername,
+  GroupCard,
+  GroupCover,
+  GroupName,
+} from "./styled";
+import { Typography } from "@mui/material";
 
-export const Home = () => {
+const popularBooks = [
+  {
+    id: 1,
+    cover: "https://picsum.photos/200/300?random=1",
+    title: "The Great Gatsby",
+    author: "F. Scott Fitzgerald",
+  },
+  {
+    id: 2,
+    cover: "https://picsum.photos/200/300?random=2",
+    title: "To Kill a Mockingbird",
+    author: "Harper Lee",
+  },
+  {
+    id: 3,
+    cover: "https://picsum.photos/200/300?random=3",
+    title: "1984",
+    author: "George Orwell",
+  },
+  {
+    id: 4,
+    cover: "https://picsum.photos/200/300?random=4",
+    title: "Pride and Prejudice",
+    author: "Jane Austen",
+  },
+  {
+    id: 5,
+    cover: "https://picsum.photos/200/300?random=5",
+    title: "The Catcher in the Rye",
+    author: "J.D. Salinger",
+  },
+];
+
+const users = [
+  {
+    id: 1,
+    avatar: "https://picsum.photos/100/100?random=1",
+    name: "John Doe",
+    username: "@johndoe",
+  },
+  {
+    id: 2,
+    avatar: "https://picsum.photos/100/100?random=2",
+    name: "Jane Smith",
+    username: "@janesmith",
+  },
+  {
+    id: 3,
+    avatar: "https://picsum.photos/100/100?random=3",
+    name: "Alice Johnson",
+    username: "@alicej",
+  },
+  {
+    id: 4,
+    avatar: "https://picsum.photos/100/100?random=4",
+    name: "Bob Williams",
+    username: "@bobw",
+  },
+  {
+    id: 5,
+    avatar: "https://picsum.photos/100/100?random=5",
+    name: "Emma Brown",
+    username: "@emmab",
+  },
+];
+
+const groups = [
+  {
+    id: "1",
+    name: "Science Fiction Enthusiasts",
+    coverImage: "https://via.placeholder.com/150?text=Sci-Fi",
+  },
+  {
+    id: "2",
+    name: "History Buffs",
+    coverImage: "https://via.placeholder.com/150?text=History",
+  },
+  {
+    id: "3",
+    name: "Fantasy Fanatics",
+    coverImage: "https://via.placeholder.com/150?text=Fantasy",
+  },
+  {
+    id: "4",
+    name: "Classic Literature Lovers",
+    coverImage: "https://via.placeholder.com/150?text=Classics",
+  },
+  {
+    id: "5",
+    name: "Mystery & Thriller",
+    coverImage: "https://via.placeholder.com/150?text=Mystery",
+  },
+  {
+    id: "6",
+    name: "Romance Reads",
+    coverImage: "https://via.placeholder.com/150?text=Romance",
+  },
+  {
+    id: "7",
+    name: "Non-Fiction Nerds",
+    coverImage: "https://via.placeholder.com/150?text=Non-Fiction",
+  },
+  {
+    id: "8",
+    name: "Young Adult Adventures",
+    coverImage: "https://via.placeholder.com/150?text=YA",
+  },
+];
+
+export const Home: React.FC = () => {
+  const handleClick = () => {
+    console.log("Clicked");
+  };
+
   return (
     <HomeContainer>
-      <Feed>Inicio</Feed>
+      <Typography
+        style={{
+          fontSize: "30px",
+          marginBottom: "20px",
+          marginTop: "20px",
+          fontWeight: "bold",
+        }}
+      >
+        ¡Bienvenido!
+      </Typography>
+      <Typography
+        style={{
+          backgroundColor: "#f5f5f5",
+          padding: "20px",
+          borderRadius: "8px",
+          boxShadow: "0 0 10px rgba(0, 0, 0, 0.2)",
+          fontSize: "18px",
+        }}
+      >
+        Descubre, organiza y comparte tus lecturas favoritas. Encuentra
+        inspiración en las reseñas de otros lectores y construye tu propio
+        rincón literario. ¡Únete a nuestra comunidad y haz que cada página
+        cuente! 📚✨
+      </Typography>
+
+      <Typography
+        style={{
+          fontSize: "24px",
+          marginBottom: "20px",
+          marginTop: "30px",
+          fontWeight: "bold",
+        }}
+      >
+        Tendencias Literarias
+      </Typography>
+      <Carousel>
+        {popularBooks.map((book) => (
+          <BookCard key={book.id}>
+            <BookCover src={book.cover} alt={book.title} />
+            <BookTitle>{book.title}</BookTitle>
+            <BookAuthor>{book.author}</BookAuthor>
+          </BookCard>
+        ))}
+      </Carousel>
+
+      <Typography
+        style={{
+          fontSize: "24px",
+          marginBottom: "20px",
+          marginTop: "30px",
+          fontWeight: "bold",
+        }}
+      >
+        Conecta y Comparte
+      </Typography>
+      <Carousel>
+        {users.map((user) => (
+          <UserCard key={user.id}>
+            <UserAvatar src={user.avatar} alt={user.name} />
+            <UserName>{user.name}</UserName>
+            <UserUsername>{user.username}</UserUsername>
+          </UserCard>
+        ))}
+      </Carousel>
+
+      <Typography
+        style={{
+          fontSize: "24px",
+          marginBottom: "20px",
+          marginTop: "30px",
+          fontWeight: "bold",
+        }}
+      >
+        Descubre Nuevos Grupos
+      </Typography>
+      <Carousel>
+        {groups.map((group) => (
+          <GroupCard key={group.id} onClick={() => handleClick()}>
+            <GroupCover src={group.coverImage} alt={group.name} />
+            <GroupName>{group.name}</GroupName>
+          </GroupCard>
+        ))}
+      </Carousel>
     </HomeContainer>
   );
 };
--- src/pages/Home/styled.tsx
+++ src/pages/Home/styled.tsx
@@ -1,16 +1,115 @@
 import styled from "styled-components";
 
 export const HomeContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  height: 80vh;
+  max-width: 800px;
+  margin: 0 auto;
+  padding: 20px;
 `;
 
-export const Feed = styled.div`
-  flex: 1;
+export const SectionTitle = styled.h2`
+  font-size: 24px;
+  margin-bottom: 20px;
+  margin-top: 30px;
+`;
+
+export const Carousel = styled.div`
   display: flex;
-  justify-content: center;
-  align-items: center;
-  font-size: 2rem;
+  overflow-x: auto;
+  gap: 20px;
+  padding-bottom: 20px;
+  background-color: #f5f5f5;
+  padding: 20px;
+  border-radius: 8px;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+`;
+
+export const BookCard = styled.div`
+  width: 200px;
+  flex-shrink: 0;
+
+  cursor: pointer;
+`;
+
+export const BookCover = styled.img`
+  width: 100%;
+  height: 300px;
+  object-fit: cover;
+  border-radius: 8px;
+`;
+
+export const BookTitle = styled.h3`
+  font-size: 16px;
+  margin: 10px 0 5px;
+`;
+
+export const BookAuthor = styled.p`
+  font-size: 14px;
+  color: #666;
+`;
+
+export const LibraryList = styled.ul`
+  list-style-type: none;
+  padding: 20px;
+  background-color: #f5f5f5;
+  border-radius: 8px;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+  margin-bottom: 40px;
+`;
+
+export const LibraryItem = styled.li`
+  font-size: 18px;
+  margin-bottom: 10px;
+`;
+
+export const UserCard = styled.div`
+  width: 150px;
+  flex-shrink: 0;
+  text-align: center;
+`;
+
+export const UserAvatar = styled.img`
+  width: 100px;
+  height: 100px;
+  border-radius: 50%;
+  object-fit: cover;
+  margin-bottom: 10px;
+  cursor: pointer;
+`;
+
+export const UserName = styled.h3`
+  font-size: 16px;
+  margin: 0;
+`;
+
+export const UserUsername = styled.p`
+  font-size: 14px;
+  color: #666;
+`;
+
+export const GroupCard = styled.div`
+  width: 200px;
+  flex-shrink: 0;
+  cursor: pointer;
+  transition: transform 0.2s ease-in-out;
+  border-radius: 8px;
+  overflow: hidden;
+  box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
+
+  &:hover {
+    transform: translateY(-5px);
+  }
+`;
+
+export const GroupCover = styled.img`
+  width: 100%;
+  height: 150px;
+  object-fit: cover;
+`;
+
+export const GroupName = styled.h3`
+  font-size: 16px;
+  font-weight: 600;
+  margin: 10px;
+  text-align: center;
   color: #333;
 `;
