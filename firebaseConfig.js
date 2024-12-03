cbf11f192c93844291884039311bc2e784883c73
MateXOre <matiasmendiola18@gmail.com>
2024-12-03 17:18:11 -0300

    fix firebase bucket

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -9,7 +9,7 @@ const firebaseConfig = {
   apiKey: "YOUR_API_KEY",
   authDomain: "YOUR_AUTH_DOMAIN",
   projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_STORAGE_BUCKET",
+  storageBucket: "buena-leida.appspot.com",
   messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
   appId: "YOUR_APP_ID",
   measurementId: "YOUR_MEASUREMENT_ID" // Optional
